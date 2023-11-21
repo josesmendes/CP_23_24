bd64579ada4f6195fb2b3aaa62ee4fe2758ead48
franciscamieiro <145159423+franciscamieiro@users.noreply.github.com>
2023-11-21 18:01:53 +0000

    Update runseq.sh

--- pg52671_pg53967/code/runseq.sh
+++ pg52671_pg53967/code/runseq.sh
@@ -3,4 +3,4 @@
 #SBATCH --ntasks=2
 #SBATCH --partition=cpar
 #SBATCH --cpus-per-task=4
-perf stat -e cycles,instructions,L1-dcache-load-misses ./MDseq.exe < ./inputdataseq.txt
+perf stat -e cycles,instructions,L1-dcache-load-misses ./MDseq.exe < inputdataseq.txt

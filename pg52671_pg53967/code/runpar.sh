d43963822d7c69b6ae0ad5a19796e2e600ca3d26
franciscamieiro <145159423+franciscamieiro@users.noreply.github.com>
2023-11-21 17:42:55 +0000

    Update runpar.sh

--- pg52671_pg53967/code/runpar.sh
+++ pg52671_pg53967/code/runpar.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-#SBATCH --time=1:00
+#SBATCH --time=3:00
 #SBATCH --ntasks=2
 #SBATCH --partition=cpar
 #SBATCH --cpus-per-task=4
-perf stat -e cycles,instructions,L1-dcache-load-misses ./MDpar.exe < inputdata.txt
-``````sh
-#!/bin/bash
-#SBATCH --time=1:00
-#SBATCH --ntasks=2
-#SBATCH --partition=cpar
-#SBATCH --cpus-per-task=4
-perf stat -e cycles,instructions,L1-dcache-load-misses ./MDpar.exe < inputdata.txt
+perf stat -e cycles,instructions,L1-dcache-load-misses ./MDpar.exe < ./inputdata.txt
